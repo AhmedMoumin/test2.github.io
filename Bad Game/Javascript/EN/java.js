ce85126cf6e8424f513b706bbcaad11f1246413d
AhmedMoumin <140558614+AhmedMoumin@users.noreply.github.com>
2023-07-26 19:50:11 +0800

    Add files via upload

---
+++ Bad Game/Javascript/EN/java.js
@@ -0,0 +1,11 @@
+function err() {
+    if (document.getElementById('input').value == "NAME") {
+        document.getElementById("err").innerHTML = " ERROR ! THE NAME CAN'T BE 'NAME' "
+    }
+}
+function FBTN() {
+    window.location.href = '/Bad Game/HTML(WithInnerCSS)/FR/fr.html' 
+}
+function ENBTN() {
+    window.location.href = '/Bad Game/index.html'
+}
---
+++ Bad Game/Javascript/FR/javafr.js
@@ -0,0 +1,5 @@
+function err() {
+    if (document.getElementById('input').value == "NAME") {
+        document.getElementById("err").innerHTML = " ERREUR ! Le Nom ne peut pas etre 'NAME' "
+    }
+}
